7294cf86d977a91a78f3d39d2fb5b939a5830cff
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-19 14:04:38 +0400

    Etalon_Task10

---
+++ 2_HomeWork/Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// 456 -> 5
+// 782 -> 8
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число: ");
+if  (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод");
+    return;
+}
+Console.WriteLine($"Введеное число {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank}");
