0f41f0141d8c490aedbd6315cce4e2dd7fec9fee
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-19 15:05:17 +0400

    Etalon_Task13

---
+++ 2_HomeWork/Task13/Program.cs
@@ -0,0 +1,36 @@
+﻿// 123456 -> 3
+// 22 -> третьей цифры нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int SearchThirdNum(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+         Console.WriteLine("Третьей цифры нет");
+         return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число: ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine($"{number} -> {SearchThirdNum(number)}");
+}
+//
